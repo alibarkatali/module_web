a2011a81580fd4be83f5913d00d83b2bdeb90c98
alibarkatali <ali.barkat.a@gmail.com>
2017-06-23 10:44:54 +0200

    théo toto

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ def rejoin():
 	playerName = data['playerName']
 	db = Db()
 
-	info = db.select("SELECT * FROM Player WHERE pl_pseudo = '@(playerName)'")
+	info = db.fetchall("SELECT * FROM Player WHERE pl_pseudo = '@(playerName)'")
 	#info = json.dumps(info)
 	#print (info[0][0])
 	#print (info['count'])
