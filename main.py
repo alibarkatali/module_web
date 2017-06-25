8a4f10b621920377dc5e4d27face41469d0fff6d
alibarkatali <ali.barkat.a@gmail.com>
2017-06-25 15:51:51 +0200

    correction bug sur getRecipeById

--- main.py
+++ main.py
@@ -381,8 +381,8 @@ def getRecipes():
 @app.route('/recipe/<int:rc_id>',methods=['GET'])
 def getRecipeById(rc_id):
 	db = Db()
-	recipe = db.select("SELECT * FROM Recipe WHERE rec_id = '"+ int(rc_id) +"'")
-	ingredient_list = db.select("SELECT ing.ing_nom FROM Ingredient ing INNER JOIN Contains con ON ing.ing_id = con.ing_id INNER JOIN Recipe rec ON rec.rec_id = con.rec_id WHERE rec.rec_id = '"+ int(rc_id) +"'")
+	recipe = db.select("SELECT * FROM Recipe WHERE rec_id = '"+ str(rc_id) +"'")
+	ingredient_list = db.select("SELECT ing.ing_nom FROM Ingredient ing INNER JOIN Contains con ON ing.ing_id = con.ing_id INNER JOIN Recipe rec ON rec.rec_id = con.rec_id WHERE rec.rec_id = '"+ str(rc_id) +"'")
 
 	if len(recipe) > 0:
 		return makeJsonResponse({ "recipe": recipe, "ingredients": ingredient_list } )
