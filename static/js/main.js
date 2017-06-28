41ed502e7c082020fc7cf436c0f7ebe38d31a5a3
alibarkatali <ali.barkat.a@gmail.com>
2017-06-28 17:01:13 +0200

    interface graphique 5

--- static/js/main.js
+++ static/js/main.js
@@ -20,24 +20,7 @@ $(document).ready(function () {
 	gameInit();
 	
 
-	/* # Mise à jour du timstamp */
-	setTimeout(function () {
-		$.ajax({
-			url: "/metrology", 
-			type: "GET",
-			contentType: 'application/json',
-			success: function(result){
-
-				$('#timer').html(result.timestamp);
-				$.each(result.weather, function( index, value ) {
-					if(value['dfn'] == 0) 
-						$('#weatherToday').html(value['weather']);
-	        		else 
-	        			$('#weatherTomorrow').html(value['weather']);
-				});
-	    	}
-		});
-	}, 100);
+	
 	
 
 	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
