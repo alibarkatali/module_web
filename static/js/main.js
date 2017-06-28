d2ae895ec90c4f5a2a056dd8308aa9b11d56fbeb
alibarkatali <ali.barkat.a@gmail.com>
2017-06-28 15:32:15 +0200

    interface graphique 3

--- static/js/main.js
+++ static/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 	gameInit();
 
 	/* # Mise à jour du timstamp */
-	setTimeout(getMetrology, 1000);
+	setTimeout(getMetrology, 100);
 
 
 	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
